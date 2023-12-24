c14837ea39d1c0569f4c6fe502cb6c288894b193
DeviV1982 <114510860+DeviV1982@users.noreply.github.com>
2023-12-25 01:30:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,22 @@
 		$password = "Password1#";
 		$dbname = "mydb";
 
-		$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+		ini_set ('error_reporting', E_ALL);
+		ini_set ('display_errors', '1');
+		error_reporting (E_ALL|E_STRICT);
 
-		// Query database for all rows in the table
-		$sql = "SELECT * FROM employees";
-		$result = $conn->query($sql);
+		$db = mysqli_init();
+		mysqli_options ($db, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 
-		if ($result->num_rows > 0) {
+		$db->ssl_set('/etc/mysql/ssl/client-key.pem', '/etc/mysql/ssl/client-cert.pem', '/etc/mysql/ssl/ca-cert.pem', NULL, NULL);
+		$link = mysqli_real_connect ($db, $servername, $username, $password , $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+if (!$link)
+{
+    die ('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error() . "\n");
+} else {
+    $result = $db->query('SELECT * FROM employees;');
+    print_r ($result);
+	if ($result->num_rows > 0) {
 			// Display table headers
 			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
 			// Loop through results and display each row in the table
@@ -35,9 +40,24 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 		} else {
 			echo "0 results";
 		}
+    $db->close();
+}
+
+		
+		//if ($result->num_rows > 0) {
+		//	// Display table headers
+	//		echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+//			// Loop through results and display each row in t0000he table/
+//			while($row = $result->fetch_assoc()) {
+//				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+//			}
+//			echo "</table>";
+//		} else {
+//			echo "0 results";
+//		}
 
 		// Close database connection
-		$conn->close();
+		//$conn->close();
 	?>
 </body>
 </html>
